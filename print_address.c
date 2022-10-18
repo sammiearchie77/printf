1503d392bbf6c6098f774e6908a8ed48a1d07c88
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-10-18 16:21:27 -0700

    Checked and edited the following files'

--- print_address.c
+++ print_address.c
@@ -3,8 +3,8 @@
 /**
  * print_address - prints address of input in hexa format
  * @l: va_list arguments from _printf
- * @f: ponter to the struct flags that determines
- * if a flag is padded to -print
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
  * Return: number of char printed
  */
 int print_address(va_list l, flags_t *f)
